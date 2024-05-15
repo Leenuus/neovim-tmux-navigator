9f6487e40ae6a09a15881ac5e0a39bf95b9ba2ae
Leenuus <linusrach24@gmail.com>
2024-05-15 18:05:44 +0800

    Remove old scripts

--- minimal-nvim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT=$(realpath "$0")
-DIR=$(dirname "$SCRIPT")
-
-export NVIM_APPNAME=vimtest
-ln -s "$(realpath "$DIR/lua/vim-tmux-navigator")" "$HOME/.config/$NVIM_APPNAME"
-nvim -c "vnew | new" "$@"
-rm "$HOME/.config/$NVIM_APPNAME"
